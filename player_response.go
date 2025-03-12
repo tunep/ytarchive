27f7c16c56539ebf7804d09193b9fc1024f67d71
Tunep <danielbper@gmail.com>
2025-03-11 20:36:21 -0400

    uncapitalized error string

--- player_response.go
+++ player_response.go
@@ -266,7 +266,7 @@ func (di *DownloadInfo) GetNewestStreamFromStreams() string {
 // Credit to yt-dlp devs for POST data and headers.
 func (di *DownloadInfo) DownloadWebPlayerResponse() (*PlayerResponse, error) {
 	if len(di.PoToken) == 0 {
-		return nil, fmt.Errorf("Cannot retrieve web api player response without a PO Token set")
+		return nil, fmt.Errorf("cannot retrieve web api player response without a PO Token set")
 	}
 	pr := &PlayerResponse{}
 	auth := GenerateSAPISIDHash(di.CookiesURL)
